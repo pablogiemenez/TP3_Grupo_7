ed13d3dcc093b8529901a317c999059d67c412bb
pablogimenez <pabloergimenez@gmail.com>
2024-09-21 14:06:16 -0300

    creación de la pantalla principal y el menú de navegación.

--- app/src/main/java/com/example/tp3_grupo_7/HomeActivity.java
+++ app/src/main/java/com/example/tp3_grupo_7/HomeActivity.java
@@ -1,23 +1,32 @@
 package com.example.tp3_grupo_7;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Obtener los datos del USUARIO que logeo.
-        String username = getIntent().getStringExtra("USERNAME");
-        String email = getIntent().getStringExtra("EMAIL");
+        // Configurar la Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // Configurar el DrawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        // Agregar el ActionBarDrawerToggle para sincronizar el DrawerLayout con el ícono del menú
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
